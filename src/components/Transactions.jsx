a44e6729ff083173aaa4fb906ed51a2eddeca814
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-03-12 05:56:16 +0100

    making the transactions table responsive on mobile screen

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -40,19 +40,20 @@ function Transactions({loadingState, navRes, navBarState, setCreateNewOptions, c
         <div className="bg-blue-40 absolute top-0 left-0 h-full w-full overflow-scroll z-50 p-3 xs:max-xl:p-0">
           {/* {navRes.transaction.length} */}
           {/* ol */}
-          <div className="bg-gray-100 rounded-lg shadow shadow-slate-600 overflow-scroll xs:max-xl:w-full ">
+          <div className="bg-gray-500 rounded-lg shadow shadow-slate-600 overflow-scroll  xs:max-xl:w-full ">
             <div className="py-5 px-3 border-b border-slate-800">
               <p className="text-slate-700 text-xl font-semibold">
                 Customers
               </p>
             </div>
             <div className="flex justify-between my-5 px-5">
-              <div className="w-[30%]">
+              <div className="w-[30%] xs:max-xl:w-10 bg-red-30 flex items-center justify-center">
                 <input
                   type="search"
-                  className="w-full py-2 px-5 bg-transparent border-[1px] border-slate-300 rounded-md text-xl"
+                  className="xs:max-xl:hidden w-full py-2 xs:max-xl:w- xs:max-xl:h-5 px-5 bg-transparent border-[1px] border-slate-300 rounded-md text-xl"
                   placeholder="Search Customers"
                 />
+                <input type="checkbox" className='xl:hidden' />
               </div>
               <div className="flex gap-5">
                 <div>
@@ -64,71 +65,71 @@ function Transactions({loadingState, navRes, navBarState, setCreateNewOptions, c
                         invoice: true,
                       });
                     }}
-                    className="p-2 w-40 rounded-md text-slate-200 font-normal bg-purple-700 hover:bg-green-600 active:bg-green-800 text-lg"
+                    className="p-2 w-40 xs:max-xl:w-32 xs:max-xl:max-w-sm rounded-md text-slate-200 font-normal bg-purple-700 hover:bg-green-600 active:bg-green-800 text-lg  xs:max-xl:text-sm"
                   >
                     New Transaction
                   </button>
                 </div>
-                <div>
-                  <button className="p-2 w-40 rounded-md text-slate-200 font-normal bg-transparent border border-purple-900 text-lg flex justify-between items-center">
-                    <p className="text-black text-xl">Actions</p>
-                    <div className="w-5 h-5 bg-red-200"></div>
+                <div> 
+                  <button className="p-2 w-40  xs:max-xl:w-28 rounded-md text-slate-200 font-normal bg-transparent border border-purple-900 text-lg flex justify-between items-center">
+                    <p className="text-black text-xl  xs:max-xl:text-sm">Actions</p>
+                    <div className="w-5 h-5 bg-red-200  xs:max-xl:w-3  xs:max-xl:h-3"></div>
                   </button>
                 </div>
               </div>
             </div>
-            <div className="px-5 py-3 flex justify-between items-center font-bold text-slate-600 text-sm bg-red-40">
+            <div className=" bg-red-400 px-5 py-3 flex justify-between items-center font-bold text-slate-600 text-sm bg-red-40">
               <div>
                 <input type="checkbox" />
               </div>
-              <div className="bg-red-20 w-[14%]">
+              <div className="bg-red-200 w-[14%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">#ID</p>
               </div>
-              <div className="bg-red-20 w-[20%] ">
+              <div className="bg-red-200 w-[20%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">
                   CUSTOMER
                 </p>
               </div>
-              <div className="bg-red-20 w-[20%]">
+              <div className="bg-red-200 w-[20%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">
                   DESCRIPTION
                 </p>
               </div>
-              <div className="bg-red-20 w-[10%]">
+              <div className="bg-red-200 w-[10%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">
                   TOTAL
                 </p>
               </div>
-              <div className="bg-red-20 w-[15%]">
+              <div className="bg-red-200 w-[15%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">
                   DATE
                 </p>
               </div>
-              <div className="bg-red-20 w-[12%]">
+              <div className="bg-red-200 w-[12%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">
                   STATUS
                 </p>
               </div>
-              <div className="bg-red-20 w-[10%]">
+              <div className="bg-red-200 w-[10%]  xs:max-xl:min-w-[8rem]">
                 <p className="border-r border-slate-400 px-5 ">
                   ACTION
                 </p>
               </div>
             </div>
-            <div className="h-80 overflow-scroll">
+            <div className="overflow-scrol">
               {navRes.transaction.map((items, index) => {
                 return (
                   <div key={items.invoiceId} className="border-b-[1px] border-slate-300 px-5 py-2 flex justify-between items-center font-bold text-slate-600 text-sm bg-red-40">
                     <div>
                       <input type="checkbox" />
                     </div>
-                    <div className="bg-red-20  w-[14%]">
+                    <div className="bg-red-200 w-[14%]  xs:max-xl:min-w-[8rem]">
                       {/* <div className="w-5 h-5 rounded-full bg-red-400"></div> */}
                       <p className="border- border-slate-400 px-5 text-lef">
                         {items.invoiceId}
                       </p>
                     </div>
-                    <div className="bg-red-20 w-[20%] flex items-center pl-3">
+                    <div className="bg-red-20 w-[20%]  xs:max-xl:min-w-[8rem] flex items-center pl-3">
                       <div className="w-5 h-5 rounded-full bg-red-900">
                         {/* <img src={} /> */}
                       </div>
@@ -139,7 +140,7 @@ function Transactions({loadingState, navRes, navBarState, setCreateNewOptions, c
                         </p>
                       </div>
                     </div>
-                    <div className="bg-red-20 w-[20%] ml-3 pl-3">
+                    <div className="bg-red-20 w-[20%]  xs:max-xl:min-w-[8rem] ml-3 pl-3">
                       <div className="">
                         <p>
                           {JSON.parse(items.products).length > 1 ? (
@@ -162,12 +163,12 @@ function Transactions({loadingState, navRes, navBarState, setCreateNewOptions, c
                         </div>
                       </div>
                     </div>
-                    <div className="bg-red-20 w-[10%] ml-3 pl-1">
+                    <div className="bg-red-20 w-[10%]  xs:max-xl:min-w-[8rem] ml-3 pl-1">
                       <p className="border- border-slate-400 truncate text-ellipsis text-base ">
                         #{items.amountTotal}
                       </p>
                     </div>
-                    <div className="bg-red-20 w-[15%] ml-3 pl-2">
+                    <div className="bg-red-20 w-[15%]  xs:max-xl:min-w-[8rem] ml-3 pl-2">
                       <p className="border- border-slate-400">
                         {items.dateIssued
                           .split("-")
@@ -175,7 +176,7 @@ function Transactions({loadingState, navRes, navBarState, setCreateNewOptions, c
                           .join("-")}
                       </p>
                     </div>
-                    <div className="bg-red-20 w-[12%]">
+                    <div className="bg-red-200 w-[12%]  xs:max-xl:min-w-[8rem]">
                       <div className="border- border-slate-400 flex ml-3 ">
                         {items.paidStatus === "paid" ? (
                           <span className="px-2 text-center py-1 bg-lime-200 text-green-500 text-sm rounded">
