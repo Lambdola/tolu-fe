1672f1a174ef4f07416641a8851aeacdf374a3c3
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2024-03-13 01:32:30 +0100

    making the customers table responsive on mobile screen

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -5,7 +5,7 @@ function Customers({ navRes, setNavItems }) {
   return (
     <div>
       <div className="bg-blue-40 absolute top-0 left-0 h-full w-full overflow-scroll z-50 p-3 xs:max-xl:p-0">
-        <div className="bg-gray-100 rounded-lg shadow shadow-slate-600 overflow-scroll ">
+        <div className="bg-gray-100  xs:max-xl:bg-white rounded-lg shadow shadow-slate-600  xs:max-xl:shadow-transparent overflow-scroll ">
           <div className="py-5 px-3 border-b border-slate-800 xs:max-xl:border-slate-300 xs:max-xl:fixed xs:max-xl:w-full">
             <p className="text-slate-700 text-xl font-semibold">Customers</p>
           </div>
@@ -35,66 +35,67 @@ function Customers({ navRes, setNavItems }) {
                 </div>
             </div>
           </div>
+          {/* =========================== */}
           <div className="xs:max-xl:bg-white bg-red-40 px-5 py-3 flex justify-between items-center font-bold text-slate-600 text-sm xs:max-xl:mt-40">
             <div>
               <input type="checkbox" />
             </div>
-            <div className="bg-red-20 w-[16%]">
-              <p className="border-r border-slate-400 px-5 ">CUSTOMERS</p>
+            <div className="bg-red-20 w-[16%] xs:max-xl:min-w-[10rem]">
+              <p className="border-r border-slate-400 px-5  xs:max-xl:pl-5 ">CUSTOMERS</p>
             </div>
-            <div className="bg-red-20 w-[16%] ">
-              <p className="border-r border-slate-400 px-5 ">PHONE</p>
+            <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[8rem]">
+              <p className="border-r border-slate-400 px-5 xs:max-xl:pl-5">PHONE</p>
             </div>
-            <div className="bg-red-20 w-[16%]">
-              <p className="border-r border-slate-400 px-5 ">EMAIL</p>
+            <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[10rem]">
+              <p className="border-r border-slate-400 px-5 xs:max-xl:pl-5">EMAIL</p>
             </div>
-            <div className="bg-red-20 w-[20%]">
-              <p className="border-r border-slate-400 px-5 ">ADDRESS</p>
+            <div className="bg-red-20 w-[20%]  xs:max-xl:min-w-[8rem]">
+              <p className="border-r border-slate-400 px-5 xs:max-xl:pl-5 ">ADDRESS</p>
             </div>
-            <div className="bg-red-20 w-[16%]">
-              <p className="border-r border-slate-400 px-5 ">TOTAL DEBT</p>
+            <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[8rem]">
+              <p className="border-r border-slate-400 px-5 xs:max-xl:pl-5">TOTAL DEBT</p>
             </div>
-            <div className="bg-red-20 w-[10%]">
-              <p className="border-r border-slate-400 px-5 ">ACTIONS</p>
+            <div className="bg-red-20 w-[10%]  xs:max-xl:min-w-[8rem]">
+              <p className="border-r border-slate-400 px-5 xs:max-xl:pl-5 ">ACTIONS</p>
             </div>
           </div>
-          <div className="h-80 overflow-scroll">
+          <div className="">
             {navRes.customers.map((items, index) => {
               return (
-                <div className="px-5 py-3 flex justify-between items-center font-bold text-slate-600 text-sm bg-red-40 border-b-[1px]  border-slate-300">
+                <div className="px-5 py-3 flex justify-between items-center font-bold text-slate-600 text-sm bg-red-40 border-b-[1px]  border-slate-300  xs:max-xl:border-transparent">
                   <div>
                     <input type="checkbox" />
                   </div>
-                  <div className="bg-red-20 w-[16%] flex items-center justify-center ml-5">
-                    <div className="w-[20%] flex items-center justify-center">
-                      <div className="w-5 h-5 rounded-full bg-red-400 flex items-center justify-center overflow-hidden">
+                  <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[10rem] flex items-center justify-center  xs:max-xl:justify-start ml-5  xs:max-xl:ml-0 xs:max-xl:pl-5">
+                    <div className="w-[20%] xs:max-xl:w-auto flex items-center justify-center">
+                      <div className="w-5 h-5 rounded-full bg-red-40 flex items-center justify-center overflow-hidden">
                         <img src={items.imageUrl} className="w-full h-full" />
                       </div>
                     </div>
 
-                    <p className="w-[80%] border-slate-400 pl-1 truncate text-ellipsis ">
+                    <p className="w-[80%] xs:max-xl:w-auto border-slate-400 pl-1 truncate text-ellipsis ">
                       {items.firstName + " " + items.lastName}
                     </p>
                   </div>
-                  <div className="bg-red-20 w-[16%] ">
+                  <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[8rem] xs:max-xl:pl-5 ">
                     <p className="border- border-slate-400 ">{items.phone} </p>
                   </div>
-                  <div className="bg-red-20 w-[16%]">
+                  <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[10rem]  xs:max-xl:pl-5">
                     <p className="border- border-slate-400 truncate text-ellipsis ">
                       {items.email}{" "}
                     </p>
                   </div>
-                  <div className="bg-red-20 w-[20%]">
+                  <div className="bg-red-20 w-[20%]  xs:max-xl:pl-5  xs:max-xl:min-w-[8rem]">
                     <p className="border- border-slate-400 px-2 truncate text-ellipsis ">
                       {items.address}{" "}
                     </p>
                   </div>
-                  <div className="bg-red-20 w-[16%]">
+                  <div className="bg-red-20 w-[16%]  xs:max-xl:min-w-[8rem]">
                     <p className="border- border-slate-400 px-5 flex gap-1 ">
                       # {items.debt ? <p>{items.debt}</p> : 0}
                     </p>
                   </div>
-                  <div className="bg-red-20 w-[10%]">
+                  <div className="bg-red-20 w-[10%] xs:max-xl:w-auto">
                     <div className="h-full w-full flex justify-start bg-blue-00 ml-5">
                       <div>
                         <MoreVertOutlined />
